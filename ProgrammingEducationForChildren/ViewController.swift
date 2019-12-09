f20fc5939f894db457a3ea3b6a0fc9de7e3ed609
masatsch <masato.11.soccer@gmail.com>
2019-12-09 13:39:00 -0800

    Initial Commit

---
+++ ProgrammingEducationForChildren/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammingEducationForChildren
+//
+//  Created by Masato TSUTSUMI on 2019/12/09.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
