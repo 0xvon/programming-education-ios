25fd58b231c972eecac96c3ac05895cd6dc33dc5
masatsch <masato.11.soccer@gmail.com>
2019-12-09 21:15:49 -0800

    wip

---
+++ ProgrammingEducationForChildren/View/Utils/Card/CardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CardViewController.swift
+//  ProgrammingEducationForChildren
+//
+//  Created by Masato TSUTSUMI on 2019/12/09.
+//  Copyright © 2019 Masato TSUTSUMI. All rights reserved.
+//
+
+import Foundation
